0b27f857b5c65c78a4ebc2d9ddabf30263888e14
Daryl Koopersmith <d@darylkoop.com>
2014-02-04 23:56:42 -0800

    Impromptu file structure.

--- js/impromptu.js
+++ js/impromptu.js
@@ -1,140 +1,134 @@
-var Impromptu, exports, fs, npmConfig, path, _;
-
+var fs = require('fs');
+var path = require('path');
 var util = require('util');
-path = require('path');
-fs = require('fs');
-_ = require('underscore');
+var _ = require('underscore');
 
-npmConfig = require('../package.json');
+var npmConfig = require('../package.json');
 
-Impromptu = (function() {
-  Impromptu.VERSION = npmConfig.version;
+function Impromptu(options) {
+  var config, ext, verbosity;
 
-  Impromptu.DEFAULT_CONFIG_DIR = process.env.IMPROMPTU_DIR || ("" + process.env.HOME + "/.impromptu");
+  this.options = options != null ? options : {};
+  config = this.options.config || Impromptu.DEFAULT_CONFIG_DIR;
+  delete this.options.config;
+  verbosity = this.options.verbosity;
+  delete this.options.verbosity;
+  this.path = {
+    config: config,
+    sources: (function() {
+      var _i, _len, _ref, _results;
 
-  function Impromptu(options) {
-    var config, ext, verbosity;
-
-    this.options = options != null ? options : {};
-    config = this.options.config || Impromptu.DEFAULT_CONFIG_DIR;
-    delete this.options.config;
-    verbosity = this.options.verbosity;
-    delete this.options.verbosity;
-    this.path = {
-      config: config,
-      sources: (function() {
-        var _i, _len, _ref, _results;
-
-        _ref = ['coffee', 'js'];
-        _results = [];
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          ext = _ref[_i];
-          _results.push("" + config + "/prompt." + ext);
-        }
-        return _results;
-      })(),
-      compiled: "" + config + "/.compiled/prompt.js",
-      log: "" + config + "/impromptu-debug.log"
-    };
-    if (this.options.serverId) {
-      this.path.serverPid = "" + config + "/.compiled/impromptu-node-server-" + this.options.serverId + ".pid";
-    }
-    this.log = new Impromptu.Log(this, verbosity);
-    this.color = new Impromptu.Color(this);
-    this.repository = new Impromptu.RepositoryFactory(this);
-    this.db = new Impromptu.DB(this);
-    this.module = new Impromptu.ModuleFactory(this);
-    this.prompt = new Impromptu.Prompt(this);
-    this._compilePrompt();
-  }
-
-  Impromptu.prototype.load = function() {
-    var err, prompt;
-
-    if (!this._compilePrompt()) {
-      return this;
-    }
-    prompt = require(this.path.compiled);
-    try {
-      if (typeof prompt.call === "function") {
-        prompt.call(this, Impromptu, this.prompt.section);
+      _ref = ['coffee', 'js'];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        ext = _ref[_i];
+        _results.push("" + config + "/prompt." + ext);
       }
+      return _results;
+    })(),
+    compiled: "" + config + "/.compiled/prompt.js",
+    log: "" + config + "/impromptu-debug.log"
+  };
+  if (this.options.serverId) {
+    this.path.serverPid = "" + config + "/.compiled/impromptu-node-server-" + this.options.serverId + ".pid";
+  }
+  this.log = new Impromptu.Log(this, verbosity);
+  this.color = new Impromptu.Color(this);
+  this.repository = new Impromptu.RepositoryFactory(this);
+  this.db = new Impromptu.DB(this);
+  this.module = new Impromptu.ModuleFactory(this);
+  this.prompt = new Impromptu.Prompt(this);
+  this._compilePrompt();
+}
+
+Impromptu.VERSION = npmConfig.version;
+Impromptu.DEFAULT_CONFIG_DIR = process.env.IMPROMPTU_DIR || ("" + process.env.HOME + "/.impromptu");
+
+Impromptu.prototype.load = function() {
+  var err, prompt;
+
+  if (!this._compilePrompt()) {
+    return this;
+  }
+  prompt = require(this.path.compiled);
+  try {
+    if (typeof prompt.call === "function") {
+      prompt.call(this, Impromptu, this.prompt.section);
+    }
+  } catch (_error) {
+    err = _error;
+    this._error('javascript', 'Your prompt file triggered a JavaScript error.', err);
+  }
+  return this;
+};
+
+Impromptu.prototype._compilePrompt = function() {
+  var coffee, compiledDir, compiledJs, compiledMtime, err, sourceMtime, sourcePrompt;
+
+  if (!(sourcePrompt = _.find(this.path.sources, function(path) {
+    return fs.existsSync(path);
+  }))) {
+    return;
+  }
+  if (fs.existsSync(this.path.compiled)) {
+    sourceMtime = fs.statSync(sourcePrompt).mtime;
+    compiledMtime = fs.statSync(this.path.compiled).mtime;
+    if (sourceMtime < compiledMtime) {
+      return true;
+    }
+  }
+  compiledDir = path.dirname(this.path.compiled);
+  if (!fs.existsSync(compiledDir)) {
+    fs.mkdir(compiledDir);
+  }
+  if (/\.js$/.test(sourcePrompt)) {
+    fs.createReadStream(sourcePrompt).pipe(fs.createWriteStream(this.path.compiled));
+    return true;
+  } else if (/\.coffee$/.test(sourcePrompt)) {
+    this._clearError('coffeescript');
+    coffee = require('coffee-script');
+    try {
+      compiledJs = coffee.compile(fs.readFileSync(sourcePrompt).toString());
+      fs.writeFileSync(this.path.compiled, compiledJs);
+      return true;
     } catch (_error) {
       err = _error;
-      this._error('javascript', 'Your prompt file triggered a JavaScript error.', err);
+      this._error('coffeescript', 'Your prompt file is not valid CoffeeScript.', err);
     }
-    return this;
-  };
+  }
+};
 
-  Impromptu.prototype._compilePrompt = function() {
-    var coffee, compiledDir, compiledJs, compiledMtime, err, sourceMtime, sourcePrompt;
-
-    if (!(sourcePrompt = _.find(this.path.sources, function(path) {
-      return fs.existsSync(path);
-    }))) {
-      return;
+Impromptu.prototype._error = function(name, content, err) {
+  this.prompt.section("error:message:" + name, {
+    content: content,
+    background: 'red',
+    foreground: 'white'
+  });
+  this.prompt.section("error:instructions:" + name, {
+    content: "\nDetails can be found in " + this.path.log + "\n",
+    options: {
+      newlines: true
     }
-    if (fs.existsSync(this.path.compiled)) {
-      sourceMtime = fs.statSync(sourcePrompt).mtime;
-      compiledMtime = fs.statSync(this.path.compiled).mtime;
-      if (sourceMtime < compiledMtime) {
-        return true;
-      }
-    }
-    compiledDir = path.dirname(this.path.compiled);
-    if (!fs.existsSync(compiledDir)) {
-      fs.mkdir(compiledDir);
-    }
-    if (/\.js$/.test(sourcePrompt)) {
-      fs.createReadStream(sourcePrompt).pipe(fs.createWriteStream(this.path.compiled));
-      return true;
-    } else if (/\.coffee$/.test(sourcePrompt)) {
-      this._clearError('coffeescript');
-      coffee = require('coffee-script');
-      try {
-        compiledJs = coffee.compile(fs.readFileSync(sourcePrompt).toString());
-        fs.writeFileSync(this.path.compiled, compiledJs);
-        return true;
-      } catch (_error) {
-        err = _error;
-        this._error('coffeescript', 'Your prompt file is not valid CoffeeScript.', err);
-      }
-    }
-  };
+  });
+  return this.log.warning("" + content + "\n\n" + err.stack + "\n----------------------------------------");
+};
 
-  Impromptu.prototype._error = function(name, content, err) {
-    this.prompt.section("error:message:" + name, {
-      content: content,
-      background: 'red',
-      foreground: 'white'
-    });
-    this.prompt.section("error:instructions:" + name, {
-      content: "\nDetails can be found in " + this.path.log + "\n",
-      options: {
-        newlines: true
-      }
-    });
-    return this.log.warning("" + content + "\n\n" + err.stack + "\n----------------------------------------");
-  };
+Impromptu.prototype._clearError = function(name) {
+  this.prompt.section("error:message:" + name, {
+    content: ''
+  });
+  return this.prompt.section("error:instructions:" + name, {
+    content: ''
+  });
+};
 
-  Impromptu.prototype._clearError = function(name) {
-    this.prompt.section("error:message:" + name, {
-      content: ''
-    });
-    return this.prompt.section("error:instructions:" + name, {
-      content: ''
-    });
-  };
-
-  return Impromptu;
-
-})();
 
 Impromptu.Error = function Error(message) {
   this.message = message
   Error.apply(this, arguments)
 }
 util.inherits(Impromptu.Error, Error)
+
 
 Impromptu.AbstractError = function AbstractError(message) {
   this.message = message;
@@ -145,31 +139,19 @@ Impromptu.AbstractError = function AbstractError(message) {
 }
 util.inherits(Impromptu.AbstractError, Impromptu.Error)
 
-exports = module.exports = Impromptu;
 
-exports.exec = require('./exec');
+Impromptu.exec = require('./exec');
+Impromptu.Color = require('./color');
+Impromptu.Cache = require('./cache/base');
+Impromptu.Cache.Shim = require('./cache/shim');
+Impromptu.Cache.Instance = require('./cache/instance');
+Impromptu.Cache.Global = require('./cache/global');
+Impromptu.Cache.Directory = require('./cache/directory');
+Impromptu.Cache.Repository = require('./cache/repository');
+Impromptu.DB = require('./db');
+Impromptu.Log = require('./log');
+Impromptu.ModuleFactory = require('./module');
+Impromptu.Prompt = require('./prompt');
+Impromptu.RepositoryFactory = require('./repository');
 
-exports.Color = require('./color');
-
-exports.Cache = require('./cache/base');
-
-exports.Cache.Shim = require('./cache/shim');
-
-exports.Cache.Instance = require('./cache/instance');
-
-exports.Cache.Global = require('./cache/global');
-
-exports.Cache.Directory = require('./cache/directory');
-
-exports.Cache.Repository = require('./cache/repository');
-
-exports.DB = require('./db');
-
-exports.Log = require('./log');
-
-exports.ModuleFactory = require('./module');
-
-exports.Prompt = require('./prompt');
-
-exports.RepositoryFactory = require('./repository');
-
+module.exports = Impromptu;
