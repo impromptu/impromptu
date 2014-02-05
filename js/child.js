904d6b354ff57fc65155cd146b5bc8c9f8818cfd
Daryl Koopersmith <d@darylkoop.com>
2014-02-04 23:24:29 -0800

    Update worker file.

--- js/child.js
+++ js/child.js
@@ -1,10 +1,7 @@
-var Impromptu, argv, buildPrompt, impromptu, minimist, parseEnv, runTests;
+var Impromptu = require('../lib/impromptu');
+var minimist = require('minimist');
 
-Impromptu = require('../lib/impromptu');
-
-minimist = require('minimist');
-
-argv = minimist(process.argv.slice(2), {
+var argv = minimist(process.argv.slice(2), {
   defaults: {
     logfile: true,
     foreground: false
@@ -15,7 +12,7 @@ argv = minimist(process.argv.slice(2), {
   }
 });
 
-impromptu = new Impromptu({
+var impromptu = new Impromptu({
   verbosity: argv.verbosity
 });
 
@@ -23,15 +20,14 @@ impromptu.log.defaultDestinations.server = argv.foreground;
 
 impromptu.log.defaultDestinations.file = argv.logfile;
 
-parseEnv = function(printenvOutput) {
-  var env, index, key, pairs, _i, _len;
-
-  env = {};
+var parseEnv = function(printenvOutput) {
+  var env = {};
   if (printenvOutput) {
-    pairs = printenvOutput.split(/(?:^|\n)([a-z0-9_]+)=/i);
+    var pairs = printenvOutput.split(/(?:^|\n)([a-z0-9_]+)=/i);
     pairs.shift();
-    for (index = _i = 0, _len = pairs.length; _i < _len; index = _i += 2) {
-      key = pairs[index];
+
+    for (var index = 0; index < pairs.length; index += 2) {
+      var key = pairs[index];
       env[key] = pairs[index + 1];
     }
   }
@@ -40,62 +36,61 @@ parseEnv = function(printenvOutput) {
 
 process.on('message', function(message) {
   if (message.type === 'env') {
-    return buildPrompt(message.data);
+    buildPrompt(message.data);
   } else if (message.type === 'test') {
-    return runTests();
+    runTests();
   }
 });
 
-buildPrompt = function(envString) {
-  var env, err;
-
-  env = parseEnv(envString);
+var buildPrompt = function(envString) {
+  var env = parseEnv(envString);
   if (env.IMPROMPTU_SHELL) {
     impromptu.options.shell = env.IMPROMPTU_SHELL;
   }
   process.env = env;
   try {
     process.chdir(env.PWD);
-  } catch (_error) {
-    err = _error;
-  }
+  } catch (e) {}
   impromptu.load();
-  return impromptu.prompt.build(function(err, results) {
+  impromptu.prompt.build(function(err, results) {
     process.send({
       type: 'end',
       data: results || env.PS1 || ("" + (process.cwd()) + " $ ")
     });
     impromptu.options.refresh = true;
-    return impromptu.prompt.build(function(err, results) {
-      return process.exit();
+    impromptu.prompt.build(function(err, results) {
+      process.exit();
     });
   });
 };
 
-runTests = function() {
-  var Mocha, file, files, fs, mocha, path, testDir, _i, _len,
-    _this = this;
-
-  path = require('path');
-  fs = require('fs');
+var runTests = function() {
   require('coffee-script');
-  Mocha = require('mocha');
+
+  var path = require('path');
+  var fs = require('fs');
+  var Mocha = require('mocha');
+
   process.setMaxListeners(0);
-  testDir = path.resolve(__dirname, '../test');
-  files = fs.readdirSync(testDir).filter(function(f) {
+
+  var testDir = path.resolve(__dirname, '../test');
+  var files = fs.readdirSync(testDir).filter(function(f) {
     return f.match(/\.coffee$/);
   });
-  mocha = new Mocha({
+
+  var mocha = new Mocha({
     reporter: 'spec'
   });
-  for (_i = 0, _len = files.length; _i < _len; _i++) {
-    file = files[_i];
+
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
     mocha.addFile(path.resolve(testDir, file));
   }
-  return mocha.run(function(failures) {
+
+  mocha.run(function(failures) {
     process.send({
       type: 'shutdown'
     });
-    return process.exit();
+    process.exit();
   });
 };
