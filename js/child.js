0e7a83ed76523b07166f21f45ab97f4960d1a534
Daryl Koopersmith <d@darylkoop.com>
2014-02-05 00:08:18 -0800

    Comments for worker file.

--- js/child.js
+++ js/child.js
@@ -24,6 +24,8 @@ var parseEnv = function(printenvOutput) {
   var env = {};
   if (printenvOutput) {
     var pairs = printenvOutput.split(/(?:^|\n)([a-z0-9_]+)=/i);
+
+    // Remove the first blank match.
     pairs.shift();
 
     for (var index = 0; index < pairs.length; index += 2) {
@@ -44,26 +46,45 @@ process.on('message', function(message) {
 
 var buildPrompt = function(envString) {
   var env = parseEnv(envString);
+
   if (env.IMPROMPTU_SHELL) {
     impromptu.options.shell = env.IMPROMPTU_SHELL;
   }
+
+  // Overload the environment.
   process.env = env;
+
+  // Update the current working directory.
   try {
     process.chdir(env.PWD);
   } catch (e) {}
+
   impromptu.load();
   impromptu.prompt.build(function(err, results) {
+    // Send back the generated prompt.
+    // If no prompt is generated, we fall back to the environment's existing prompt.
+    // As a result, by registering no prompt sections, Impromptu can be used strictly
+    // for its background updating capabilities. If PS1 exists, falls back to "`pwd` $".
     process.send({
       type: 'end',
       data: results || env.PS1 || ("" + (process.cwd()) + " $ ")
     });
+
+    // Run the background update.
+    // We synchronously perform the background update to optimize for speed of prompt
+    // generation. Reusing the process allows us to conserve memory while the socket
+    // server is idling.
     impromptu.options.refresh = true;
+
+    // Rebuild the prompt to refresh the cache.
     impromptu.prompt.build(function(err, results) {
       process.exit();
     });
   });
 };
 
+// Tests
+// FYI: This is ridiculous.
 var runTests = function() {
   require('coffee-script');
 
@@ -71,6 +92,7 @@ var runTests = function() {
   var fs = require('fs');
   var Mocha = require('mocha');
 
+  // Allow unlimited listeners for tests.
   process.setMaxListeners(0);
 
   var testDir = path.resolve(__dirname, '../test');
