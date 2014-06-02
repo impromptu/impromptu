c1bc0c32fa688c64754fc590aa12c645785c6d69
Daryl Koopersmith <d@darylkoop.com>
2014-06-01 19:27:10 -0700

    Improve our externs for async.

--- externs/async.js
+++ externs/async.js
@@ -1,9 +1,47 @@
-// The subset of the Async API that we use.
-var async = {
-  each: function() {},
-  every: function() {},
-  map: function() {},
-  parallel: function() {},
-  series: function () {},
-  waterfall: function () {}
-}
+/**
+ * @fileoverview The subset of the async API that we use.
+ */
+
+/** @type {Object} */
+var async = {}
+
+/**
+ * @param {Array} arr
+ * @param {function(*,function(Error))} iterator
+ * @param {function(Error)} callback
+ */
+async.each = function (arr, iterator, callback) {}
+
+/**
+ * @param {Array} arr
+ * @param {function(*,function(Error, *))} iterator
+ * @param {function(Error, Array)} callback
+ */
+async.map = function (arr, iterator, callback) {}
+
+/**
+ * @param {Array} arr
+ * @param {function(*,function(boolean))} iterator
+ * @param {function(boolean)} callback
+ */
+async.every = function (arr, iterator, callback) {}
+
+/**
+ * @param {Array.<function(Error,*)>|Object.<function(Error,*)>} tasks
+ * @param {function(Error,Array)=} callback
+ */
+async.series = function (tasks, callback) {}
+
+/**
+ * @param {Array.<function(Error,*)>|Object.<function(Error,*)>} tasks
+ * @param {function(Error,Array)=} callback
+ */
+async.parallel = function (tasks, callback) {}
+
+/**
+ * @param {Array.<function(Error,...[?])>|Object.<function(Error,...[?])>} tasks
+ * @param {function(Error,Array=)=} callback
+ */
+async.waterfall = function (tasks, callback) {}
+
+module.exports = async
